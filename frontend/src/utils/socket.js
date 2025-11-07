721b1ddd428a8a8449ff5c7423e97e73528b554e
Gowsalya2572 <87688391+Gowsalya2572@users.noreply.github.com>
2025-11-07 19:34:26 +0530

    updated

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL || "http://localhost:4000" || "https://slot-swapper-backend-six.vercel.app/api", {
+const socket = io("https://slot-swapper-backend-six.vercel.app/api", {
   autoConnect: false,
 });
 
